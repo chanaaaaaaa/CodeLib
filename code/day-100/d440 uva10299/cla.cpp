f4a0f8802d950a70bad0ca015b5c8701f76bab44
chanaaaaaaa <king960129@gmail.com>
2026-01-10 14:32:53 +0800

    wrc_2026/01/10-14.32_mdfkIFinallyMadeTheGitignore

---
+++ code/day-100/d440 uva10299/cla.cpp
@@ -0,0 +1,90 @@
+#pragma GCC optimize(1)
+#pragma GCC optimize(2)
+#pragma GCC optimize(3)
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("inilne")
+#pragma GCC optimize("-fgcse")
+#pragma GCC optimize("fast-math")
+#pragma GCC optimize("-fgcse-lm")
+#pragma GCC optimize("-fipa-sra")
+#pragma GCC optimize("-ftree-vrp")
+#pragma GCC optimize("-fpeephole2")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-fsched-spec")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("-falign-labels")
+#pragma GCC optimize("-fdevirtualize")
+#pragma GCC optimize("-fcaller-saves")
+#pragma GCC optimize("-fcrossjumping")
+#pragma GCC optimize("-fthread-jumps")
+#pragma GCC optimize("-funroll-loops")
+#pragma GCC optimize("-fwhole-program")
+#pragma GCC optimize("-freorder-blocks")
+#pragma GCC optimize("-fschedule-insns")
+#pragma GCC optimize("inline-functions")
+#pragma GCC optimize("-ftree-tail-merge")
+#pragma GCC optimize("-fschedule-insns2")
+#pragma GCC optimize("-fstrict-aliasing")
+#pragma GCC optimize("-fstrict-overflow")
+#pragma GCC optimize("-falign-functions")
+#pragma GCC optimize("-fcse-follow-jumps")
+#pragma GCC optimize("-fsched-interblock")
+#pragma GCC optimize("-fpartial-inlining")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("-freorder-functions")
+#pragma GCC optimize("-findirect-inlining")
+#pragma GCC optimize("-ferun-cse-after-loop")
+#pragma GCC optimize("inline-small-functions")
+#pragma GCC optimize("-finline-small-functions")
+#pragma GCC optimize("-ftree-switch-conversion")
+#pragma GCC optimize("-foptimize-sibling-calls")
+#pragma GCC optimize("-fexpensive-optimizations")
+#pragma GCC optimize("-funsafe-loop-optimizations")
+#pragma GCC optimize("inline-function-called-once")
+#pragma GCC optimize("-fdelete-null-pointer-checks")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+//#pragma comment(linker, "/stack:200000000")
+#include <cstdio>
+#include <vector>
+#include <set>
+//using namespace std;
+
+inline int read(){
+    int x=0;
+    char c=0;
+    while(c<'0'||c>'9'){
+        c=getchar();
+        if(c==-1){return 0;}
+    }
+    while(c>='0'&&c<='9'){
+        x=(x<<3)+(x<<1)+(c-'0');
+        c=getchar();
+    }
+    return x;
+}
+inline void write(int x){
+    if(x>=10){write(x/10);}
+    putchar(x%10+'0');
+}
+//-block
+inline int phi(int x){
+    if(x<2){return 0;}
+    int ret=x;
+    for(int p=2;p*p<=x;++p){
+        if(x%p==0){
+            while(x%p==0){x/=p;}
+            ret-=ret/p;
+        }
+        if(x==1){break;}
+    }
+    if(x>1){ret-=ret/x;}
+    return ret;
+}
+signed main(){
+    int N;
+    while(N=read()){
+        write(phi(N));
+        putchar('\n');
+    }
+    return 0;
+}
