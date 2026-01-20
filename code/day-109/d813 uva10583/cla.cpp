1da3b5df75f538a1c90130db6e9323c77499f642
chanaaaaaaa <king960129@gmail.com>
2026-01-20 22:56:14 +0800

    wrc_2026/01/20-22.55_home

---
+++ code/day-109/d813 uva10583/cla.cpp
@@ -0,0 +1,110 @@
+#pragma GCC optimize(1)
+#pragma GCC optimize(2)
+#pragma GCC optimize(3)
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("inline")
+#pragma GCC optimize("-fgcse")
+#pragma GCC optimize("fast-math")
+#pragma GCC optimize("-fgcse-lm")
+#pragma GCC optimize("-fipa-sra")
+#pragma GCC optimize("-ftree-vrp")
+#pragma GCC optimize("-fpeephole2")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-fsched-spec")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("-falign-labels")
+#pragma GCC optimize("-fdevirtualize")
+#pragma GCC optimize("-fcaller-saves")
+#pragma GCC optimize("-fcrossjumping")
+#pragma GCC optimize("-fthread-jumps")
+#pragma GCC optimize("-funroll-loops")
+#pragma GCC optimize("-fwhole-program")
+#pragma GCC optimize("-freorder-blocks")
+#pragma GCC optimize("-fschedule-insns")
+#pragma GCC optimize("inline-functions")
+#pragma GCC optimize("-ftree-tail-merge")
+#pragma GCC optimize("-fschedule-insns2")
+#pragma GCC optimize("-fstrict-aliasing")
+#pragma GCC optimize("-fstrict-overflow")
+#pragma GCC optimize("-falign-functions")
+#pragma GCC optimize("-fcse-follow-jumps")
+#pragma GCC optimize("-fsched-interblock")
+#pragma GCC optimize("-fpartial-inlining")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("-freorder-functions")
+#pragma GCC optimize("-findirect-inlining")
+#pragma GCC optimize("-ferun-cse-after-loop")
+#pragma GCC optimize("inline-small-functions")
+#pragma GCC optimize("-finline-small-functions")
+#pragma GCC optimize("-ftree-switch-conversion")
+#pragma GCC optimize("-foptimize-sibling-calls")
+#pragma GCC optimize("-fexpensive-optimizations")
+#pragma GCC optimize("-funsafe-loop-optimizations")
+#pragma GCC optimize("inline-function-called-once")
+#pragma GCC optimize("-fdelete-null-pointer-checks")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+//#pragma comment(linker, "/stack:200000000")
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+inline int read(){
+    int x=0;
+    char c=0;
+    while(c<'0'||c>'9'){
+        c=getchar();
+        if(c==-1){return 0;}
+    }
+    while(c>='0'&&c<='9'){
+        x=(x<<3)+(x<<1)+(c-'0');
+        c=getchar();
+    }
+    return x;
+}
+inline void write(int x){
+    if(x>=10){write(x/10);}
+    putchar(x%10+'0');
+}
+//-block
+vector<int>F;
+inline int FindRoot(int x){
+    if(F[x]==x){return x;}
+    return F[x]=FindRoot(F[x]);
+}
+inline bool Union(int a,int b){
+    a=FindRoot(a);
+    b=FindRoot(b);
+    if(a==b){
+        return false;
+    }else{
+        F[b]=a;
+        return true;
+    }
+}
+signed main(){
+    int N,M,caese=0;
+    while(N=read()){
+        M=read();
+        if(N+M==0){break;}
+        F.clear();
+        for(int i=0;i<=N;++i){
+            F.emplace_back(i);
+        }
+        int res=N;
+        while(M--){
+            int a=read(),b=read();
+            if(Union(a,b)){--res;}
+        }
+        putchar('C');
+        putchar('a');
+        putchar('s');
+        putchar('e');
+        putchar(' ');
+        write(++caese);
+        putchar(':');
+        putchar(' ');
+        write(res);
+        putchar('\n');
+    }
+    return 0;
+}
---
+++ code/day-109/uva10579/cla.cpp
@@ -0,0 +1,107 @@
+#pragma GCC optimize(1)
+#pragma GCC optimize(2)
+#pragma GCC optimize(3)
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("inline")
+#pragma GCC optimize("-fgcse")
+#pragma GCC optimize("fast-math")
+#pragma GCC optimize("-fgcse-lm")
+#pragma GCC optimize("-fipa-sra")
+#pragma GCC optimize("-ftree-vrp")
+#pragma GCC optimize("-fpeephole2")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-fsched-spec")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("-falign-labels")
+#pragma GCC optimize("-fdevirtualize")
+#pragma GCC optimize("-fcaller-saves")
+#pragma GCC optimize("-fcrossjumping")
+#pragma GCC optimize("-fthread-jumps")
+#pragma GCC optimize("-funroll-loops")
+#pragma GCC optimize("-fwhole-program")
+#pragma GCC optimize("-freorder-blocks")
+#pragma GCC optimize("-fschedule-insns")
+#pragma GCC optimize("inline-functions")
+#pragma GCC optimize("-ftree-tail-merge")
+#pragma GCC optimize("-fschedule-insns2")
+#pragma GCC optimize("-fstrict-aliasing")
+#pragma GCC optimize("-fstrict-overflow")
+#pragma GCC optimize("-falign-functions")
+#pragma GCC optimize("-fcse-follow-jumps")
+#pragma GCC optimize("-fsched-interblock")
+#pragma GCC optimize("-fpartial-inlining")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("-freorder-functions")
+#pragma GCC optimize("-findirect-inlining")
+#pragma GCC optimize("-ferun-cse-after-loop")
+#pragma GCC optimize("inline-small-functions")
+#pragma GCC optimize("-finline-small-functions")
+#pragma GCC optimize("-ftree-switch-conversion")
+#pragma GCC optimize("-foptimize-sibling-calls")
+#pragma GCC optimize("-fexpensive-optimizations")
+#pragma GCC optimize("-funsafe-loop-optimizations")
+#pragma GCC optimize("inline-function-called-once")
+#pragma GCC optimize("-fdelete-null-pointer-checks")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+//#pragma comment(linker, "/stack:200000000")
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+inline int read(){
+    int x=0;
+    char c=0;
+    while(c<'0'||c>'9'){
+        c=getchar();
+        if(c==-1){return 0;}
+    }
+    while(c>='0'&&c<='9'){
+        x=(x<<3)+(x<<1)+(c-'0');
+        c=getchar();
+    }
+    return x;
+}
+inline void write(int x){
+    if(x>=10){write(x/10);}
+    putchar(x%10+'0');
+}
+//-block
+vector<vector<int>>dat;
+inline vector<int> add(vector<int>&a,vector<int>&b){
+    vector<int>tmp(max(a.size(),b.size())+1,0);
+    for(int i=0;i<a.size();++i){
+        tmp[i]+=a[i];
+    }
+    for(int i=0;i<b.size();++i){
+        tmp[i]+=b[i];
+        tmp[i+1]+=tmp[i]/10;
+        tmp[i]%=10;
+    }
+    while(tmp.back()==0 && tmp.size()>1){
+        tmp.pop_back();
+    }
+    return tmp;
+}
+inline void form(){
+    dat.assign(3,vector<int>(1,1));
+    for(int i=3;i<=4786;++i){
+        dat.push_back(add(dat[i-1],dat[i-2]));
+    }
+    return;
+}
+inline void print(int x){
+    for(int i=dat[x].size()-1;i>=0;--i){
+        putchar(dat[x][i]+'0');
+    }
+    putchar('\n');
+}
+
+signed main(){
+    form();
+    int N;
+    while(N=read()){
+        print(N);
+    }
+    return 0;
+}
